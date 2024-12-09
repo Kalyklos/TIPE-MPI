278c7bde0872790b45524a20dcd237e121ea7cf1
Kalyklos <matthebg73@gmail.com>
2024-12-09 16:51:22 +0100

    docstring finished

--- battle.py
+++ battle.py
@@ -1,3 +1,5 @@
+"""In this module, all int are positive. The goal of this module is to implement function to create and administrate a battlefield.  """
+
 from stack_v import *
 import random
 random.seed() #to give something that seem more random to use the random function (cause the random is hardcode)
@@ -27,7 +29,6 @@ class Effect:
         crea.actual_life += nb
         crea.actual_strength += nb
         return
-    def 
 
 def effect (left, effect_add):
     if perma_boost in effect_add.key:
@@ -80,6 +81,12 @@ class Card:
 
 class Battlefield:
     def __init__ (self, deck_j_left, deck_j_right):
+        """Init of the Battlefield class
+
+        Args:
+            deck_j_left (dict): a magic deck
+            deck_j_right (dict): a magic deck
+        """
         self.end = False
         self.winner = -1
         self.life_j_left, self.life_j_right = 20
@@ -105,15 +112,15 @@ class Battlefield:
         self.trigger_effect_upkeep_left, self.trigger_effect_upkeep_right = []
         self.trigger_effect_end_step_left, self.trigger_effect_end_step_right = []
     def is_playable (self, left, card):
-    """A function to calcul if the card card is actually playable or not.
+        """A function to calcul if the card card is actually playable or not.
 
-    Args:
-        left (bool): To know which player want to play (True = left)
-        card (Card): The card to know if it playable
+        Args:
+            left (bool): To know which player want to play (True -> left)
+            card (Card): The card to know if it playable
 
-    Returns:
-        bool : If the card is playable then True
-    """
+        Returns:
+            bool : If the card is playable : True
+        """
         if left:
             for symbol in card.cost:
                 if (self.mana_by_crea_left + self.nb_land_in_play_left - self.mana_used_left) <= card.cost[symbol]:
@@ -131,7 +138,7 @@ class Battlefield:
         """A function to draw n card.
 
         Args:
-            left (bool): if it's the left player who need to draw then True
+            left (bool): the player who need to draw (True -> left)
             n (int)): the number of card to draw
         """
         if left:
@@ -147,6 +154,11 @@ class Battlefield:
         return
 
     def upkeep (self, left):
+        """a function to do the upkeep step (untap, upkeep triggers, draw)
+
+        Args:
+            left (bool): the player who is in upkeep phase (True -> left)
+        """
         untap_step (self, left)
         if left:
             for eff in self.trigger_effect_upkeep_left:
@@ -157,6 +169,8 @@ class Battlefield:
         draw (self, left, 1)
 
     def clean_step (self):
+        """a function tu od the clean_step (end of the endstep)
+        """
         for crea in creature_j_left:
             crea.actual_life = crea.life
             crea.actual_strength = crea.strength
@@ -166,6 +180,12 @@ class Battlefield:
         return
 
     def discard (self, left, n):
+        """a function to discard cards (use in the end_step function)
+
+        Args:
+            left (bool): the player who need to discard (True -> left)
+            n (int): the number of card to discard
+        """
         if left:
             shuffle (self.hand_j_left)
             for i in range (n):
@@ -176,6 +196,14 @@ class Battlefield:
             hand_j_right.pop()
 
     def end_step (self, left):
+        """a function to do the end step phase, whoes need the discard and clean_step functions
+
+        Args:
+            left (bool): the player who is in his end step (True -> left)
+
+        Returns:
+            None: the function doesn't return anything, the return is here only to stop the function
+        """
         if left:
             for eff in trigger_effect_end_step_left:
                 eff.trigger ()
@@ -188,20 +216,33 @@ class Battlefield:
             discard (self, left, (len(self.hand_j_right) - self.hand_size_right))
         return clean_step ()      
 
-    def play_land (self, left, n):
+    def play_land (self, left):
+        """a function to play a single land
+
+        Args:
+            left (_type_): the player who want to play a land (True -> left)
+        """
         land_in_hand = []
         if left:
             for c in self.hand_j_left:
                 if c.c_type == "land":
                     self.nb_land_in_play_left +=1
                     self.hand_j_left.remove(c)
-                    return
+                    return                       #the return stop the funtcion in order to not play another land
         for c in self.hand_j_right:
             if c.c_type == "land":
                 self.nb_land_in_play_right +=1
                 self.hand_j_right.remove(c)
-                return
+                return                           #the return stop the funtcion in order to not play another land
     def possible_attacking_creature(self, left):
+        """a function to know which creature are able to attack
+
+        Args:
+            left (bool): the player who want to attack (True -> left)
+
+        Returns:
+            tab: a python array with the creature ble to attack for the chosen player
+        """
         tab = []
         if left:
             for crea in board_j_left:
@@ -213,14 +254,22 @@ class Battlefield:
                     tab.append(crea)
         return tab
     def is_finish (self):
+        """a function to know if the game is finished
+
+        Returns:
+            int: 0 if the right player have win, 1 if the right player have win, else -1
+        """
         if self.life_j_left < 1:
             return 0
         if self.life_j_right < 1:
             return 1
         return -1
-    def win_the_game (self, left):
-        return left
     def untap_step (self, left):
+        """a function to do the untap step
+
+        Args:
+            left (bool): to know which player in his untap_step (True -> left)
+        """
         if left:
             self.mana_used_left = 0
             for crea in self.creature_j_left:
@@ -240,18 +289,35 @@ class Battlefield:
             if crea.can_untap:
                 crea.is_tap = False
         return
-    def main_phase (self, left, eff):
+    def main_phase (self, left):
+        """a function to do a main phase
+
+        Args:
+            left (bool): to know which player is in his main phase (True -> left)
+        """
         if left:
             self.can_cast_sorcery_left = True
         else:
             self.can_cast_sorcery_right = True
     def new_creature (self, left, crea):
+        """a function to add a creature to a board
+
+        Args:
+            left (bool): the player who want to add a creature (True -> left)
+            crea (Creature): the creature to add
+        """
         if left:
             self.creature_j_left.append(crea)
             return
         self.creature_j_right.append(crea)
         return
     def play_a_card (self, card, left):
+        """a function to play the card card
+
+        Args:
+            card (Card): a magic card from the Card class
+            left (bool): to know which player want to play (True -> left)
+        """
         mana_needed = 0
         for symbol in card.cost:
             mana_needed += card.cost[symbol]
@@ -259,7 +325,7 @@ class Battlefield:
             self.mana_used_left += mana_needed
             hand_j_left.remove(card)
             if self.mana_used_left > self.nb_land_in_play_left:
-                #to do
+                #to do with mana creatures
                 pass
         else:
             hand_j_right.remove(card)
@@ -267,12 +333,19 @@ class Battlefield:
         return
     
     def game_begin (self):
+        """a function to init the game
+        """
         random.shuffle(self.deck_j_left)
         random.shuffle(self.deck_j_right)
         draw (True, 7)
         draw (False, 7)
-        return randint(1, 2) == 1
-    def new_turn (self, first, left):
+        return
+    def new_turn (self, left):
+        """a function to begin a new turn
+
+        Args: 
+            left (bool): to know which player is begining his turn (True -> left)
+        """
         if left:
             for i in self.upkeep_left:
                 eff_card(i, left)
@@ -283,6 +356,11 @@ class Battlefield:
         untap (left)
         return
     def dying_creature (self, tab):
+        """a function to update the python list tab, adding the died creature
+
+        Args:
+            tab (tab): a python list initially empty
+        """
         for i in range (len(self.creature_j_left) -1):
             if self.creature_j_left[i].actual_life < 1:
                 tab.append(self.creature_j_left.pop([i]))
@@ -293,16 +371,30 @@ class Battlefield:
 
 class Combat_phase:
     def __init__ (self, actual_battlefield):
+        """the init of the Combat_phase class
+
+        Args:
+            actual_battlefield (Battlefield): the battlefield to manage the combat phase
+        """
         self.actual_battlefield = actual_battlefield
         self.attacking_creature = {}
         self.died_creature = []
     def reset (self):
+        """a function to reset the combat phase
+        """
         self.attacking_creature = {}
         self.died_creature = []
     def attack_phase (self, attacking_creature_tab):
+        """a function to declare the attacking creature
+
+        Args:
+            attacking_creature_tab (tab): a python tab to declare the attacking creature
+        """
         for crea in attacking_creature_tab:
             self.attacking_creature[crea] = []
     def assign_damage (self):
+        """a function to assign the damage after the block phase
+        """
         for crea in self.attacking_creature.keys:
             if attacking_creature[crea] == []:
                 if crea.owner:
@@ -314,10 +406,14 @@ class Combat_phase:
                 self.attacking_creature[crea].actual_life -= crea.actual_strength
         dying_creature (self.died_creature)
     def died_effect (self):
+        """a function to execute the died effect
+        """
         for crea in died_creature:
             c = died_creature.pop()
             c.died_effect()
     def finish (self):
+        """a function to actualise if wheather or not the duel is finished
+        """
         if actual_battlefield.is_finish == -1:
             actual_battlefield.end = True
 
